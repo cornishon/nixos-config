bc9b478e67ac2d3150c6ceb8b21a80c99f6e227e
Adam Zadrożny <zadroznyadam@pm.me>
2023-07-04 16:37:08 +0200

    (asus-tuf) fix for system sleep

--- system/machine/asus-tuf/default.nix
+++ system/machine/asus-tuf/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
@@ -19,6 +19,22 @@
     '';
   };
   services.supergfxd.enable = false;
+  environment.systemPackages = with pkgs; [ zenstates msr kmod microcodeAmd ];
+
+  systemd.services."disable-c6" = {
+    unitConfig = {
+      Description = "Ryzen Disable C6 (fixes suspend to ram)";
+      DefaultDependencies = "no";
+      After = "sysinit.target local-fs.target suspend.target hibernate.target";
+      Before = "basic.target";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.kmod}/bin/modprobe msr";
+      ExecStart = "${pkgs.zenstates}/bin/zenstates --c6-disable";
+    };
+    wantedBy = [ "basic.target" "suspend.target" "hibernate.target" ];
+  };
 
   # Setup keyfile
   boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
