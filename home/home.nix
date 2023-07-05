c47442bbe4e42f7d7872aa9a418f0147c025f1a4
Adam Zadrożny <zadroznyadam@pm.me>
2023-07-05 15:22:03 +0200

    Split from "split home.packages"

--- home/home.nix
+++ home/home.nix
@@ -1,30 +1,33 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   username = "adamz";
   homeDirectory = "/home/${username}";
   configHome = "${homeDirectory}/.config";
-  packages = with pkgs; [
+
+  gnomeExtensions = with pkgs.gnomeExtensions; [
+    appindicator
+    just-perfection
+    scroll-panel
+    tophat
+    night-theme-switcher
+    power-profile-switcher
+  ];
+
+  myPackages = with pkgs; [
     any-nix-shell # fish support for nix shell
     exa # ls replacement
     fd # find replacement
-    gnomeExtensions.appindicator
-    gnomeExtensions.just-perfection
-    gnomeExtensions.scroll-panel
-    gnomeExtensions.tophat
-    gnomeExtensions.night-theme-switcher
-    gnomeExtensions.power-profile-switcher
     blackbox-terminal # for gnome
-    iosevka-comfy.comfy # comfy font for the terminal
     killall # kill all processes matching a name
     libnotify # notify-send command
     nixfmt # formatting for nix code
     playerctl # music player controlle
     ripgrep # fast grep
-    rustup # for rust development
     tldr # summary of a man page
     tree # display files in a tree view
     zellij # terminal workspace
   ];
+
 in {
   programs.home-manager.enable = true;
 
@@ -42,7 +45,9 @@ in {
   };
 
   home = {
-    inherit username homeDirectory packages;
+    inherit username homeDirectory;
+
+    packages = myPackages ++ gnomeExtensions;
 
     sessionVariables = { };
 
